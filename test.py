621904efdbc868a631c86048c9c91157a8d23720
Saiping Zhang <1145178427@qq.com>
2022-01-31 22:24:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -79,7 +79,31 @@ def main():
             input_data = input_data.cuda()
 
             # If you do not have enough memory, you may need to split the input_data
-            # into few parts and enhance each part and merge them finally.
+            # into few parts and enhance each part and merge them finally. 
+            # How to split the input_data depends on the sequence resolution and your CUDA memory. 
+            # For example, when implemented on NVIDIA 2080ti, we do not need to split sequences in Class C and D.
+            # But we split sequences in Class A as
+            # input_data1 = input_data[:, :, :, :256]
+            # input_data2 = input_data[:, :, :, 256:256*2]
+            # input_data3 = input_data[:, :, :, 256*2:256*3]
+            # input_data4 = input_data[:, :, :, 256*3:256*4]
+            # input_data5 = input_data[:, :, :, 256*4:256*5]
+            # input_data6 = input_data[:, :, :, 256*5:256*6]
+            # input_data7 = input_data[:, :, :, 256*6:256*7]
+            # input_data8 = input_data[:, :, :, 256*7:256*8]
+            # input_data9 = input_data[:, :, :, 256*8:256*9]
+            # input_data10 = input_data[:, :, :,256*9:256*10]
+            # For sequences in Class B:
+            # input_data1 = input_data[:, :, :, :320]
+            # input_data2 = input_data[:, :, :, 320:320*2]
+            # input_data3 = input_data[:, :, :, 320*2:320*3]
+            # input_data4 = input_data[:, :, :, 320*3:320*4]
+            # input_data5 = input_data[:, :, :, 320*4:320*5]
+            # input_data6 = input_data[:, :, :, 320*5:320*6]
+            # For sequences in Class E:
+            # input_data1 = input_data[:, :, :, :640]
+            # input_data2 = input_data[:, :, :, 640:]
+            
 
             # enhance
             with torch.no_grad():
@@ -87,6 +111,34 @@ def main():
                 # torch.tensor([2]): QP 32 torch.tensor([3]): QP 37
                 qp_num = torch.tensor([3]).unsqueeze(0).to(0)
                 enhanced_frm = model(opts_dict['radius'], input_data, qp_num)
+                # For sequences in Class A, we enhance each part indepently and then merge them. 
+                # enhanced_frm_1 = model(opts_dict['radius'], input_data1,qp_num)
+                # enhanced_frm_2 = model(opts_dict['radius'], input_data2,qp_num)
+                # enhanced_frm_3 = model(opts_dict['radius'], input_data3,qp_num)
+                # enhanced_frm_4 = model(opts_dict['radius'], input_data4,qp_num)
+                # enhanced_frm_5 = model(opts_dict['radius'], input_data5,qp_num)
+                # enhanced_frm_6 = model(opts_dict['radius'], input_data6,qp_num)
+                # enhanced_frm_7 = model(opts_dict['radius'], input_data7,qp_num)
+                # enhanced_frm_8 = model(opts_dict['radius'], input_data8,qp_num)
+                # enhanced_frm_9 = model(opts_dict['radius'], input_data9,qp_num)
+                # enhanced_frm_10 = model(opts_dict['radius'], input_data10,qp_num)
+                # enhanced_frm = torch.cat((enhanced_frm_1,enhanced_frm_2,enhanced_frm_3,
+                #                     enhanced_frm_4,enhanced_frm_5,enhanced_frm_6,
+                #                     enhanced_frm_7,enhanced_frm_8,enhanced_frm_9,
+                #                     enhanced_frm_10),dim=3)
+                # For sequences in Class B:
+                # enhanced_frm_1 = model(opts_dict['radius'], input_data1,qp_num)
+                # enhanced_frm_2 = model(opts_dict['radius'], input_data2,qp_num)
+                # enhanced_frm_3 = model(opts_dict['radius'], input_data3,qp_num)
+                # enhanced_frm_4 = model(opts_dict['radius'], input_data4,qp_num)
+                # enhanced_frm_5 = model(opts_dict['radius'], input_data5,qp_num)
+                # enhanced_frm_6 = model(opts_dict['radius'], input_data6,qp_num)
+                # enhanced_frm = torch.cat((enhanced_frm_1,enhanced_frm_2,enhanced_frm_3,
+                #                     enhanced_frm_4,enhanced_frm_5,enhanced_frm_6),dim=3)
+                # For sequences in Class E:
+                # enhanced_frm_1 = model(opts_dict['radius'], input_data1,qp_num)
+                # enhanced_frm_2 = model(opts_dict['radius'], input_data2,qp_num)
+                # enhanced_frm = torch.cat((enhanced_frm_1,enhanced_frm_2),dim=3)
 
             data = enhanced_frm.detach().cpu().numpy()
             data = np.clip(data, 0, 1)
